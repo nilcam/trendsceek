c463e5a054de6ff6b662885a108da374a736b701
nilcam <111294284+nilcam@users.noreply.github.com>
2022-08-15 16:29:26 +0200

    Compatible with spatstat 2.3-4
    spatstat functions/objects Emark, markcorr, Vmark, markvario, envelope now under spatstat.core::
    spatstat functions rlabel now under spatstat.random::

--- R/trendsceek.R
+++ R/trendsceek.R
@@ -330,7 +330,7 @@ gen_null <- function(pp, n.rand = 2000){
     ##List of pps with fixed positions but randomized labels
 
     perm = TRUE #keep the marginal mark dist the same
-    pp.perm.list = lapply(1:n.rand, function(j.it, pp, permute){spatstat::rlabel(pp, permute = permute)}, pp = pp, permute = perm)    
+    pp.perm.list = lapply(1:n.rand, function(j.it, pp, permute){spatstat.random::rlabel(pp, permute = permute)}, pp = pp, permute = perm)    
     
     return(pp.perm.list)
 }
@@ -339,7 +339,7 @@ calc_pp_trendstats <- function(pp, pp.perm.list, alpha_env = 0.05, alpha_nom_ear
 ###Calculate test statistic of obs and null
     
     ##available test stat methods
-    fcns = c(spatstat::Emark, spatstat::markcorr, spatstat::markvario, spatstat::Vmark)
+    fcns = c(spatstat.core::Emark, spatstat.core::markcorr, spatstat.core::markvario, spatstat.core::Vmark)
     names(fcns) = c('Emark', 'markcorr', 'markvario', 'Vmark')
 
     ##subset on selected methods
@@ -399,7 +399,7 @@ calc_pp_trendstats_jmethod <- function(pp, pp.perm.list, j.fcn, alpha_env = 0.05
         ##get local (per radius) test stats for subset
         ##This is the time-consuming step
         j_nsim = length(j_pp_perm_list)
-        j_localstats = spatstat::envelope(pp, j.fcn, nsim = j_nsim, simulate = j_pp_perm_list, global = FALSE, nsim2 = 0, savefuns = TRUE, verbose = FALSE)
+        j_localstats = spatstat.core::envelope(pp, j.fcn, nsim = j_nsim, simulate = j_pp_perm_list, global = FALSE, nsim2 = 0, savefuns = TRUE, verbose = FALSE)
         
         ##add test-stats from previous subsets
         local.stats.list = add_subsetstats(local.stats.list, j_localstats, j_nsim_it)
